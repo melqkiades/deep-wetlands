55a58c7a70e7162f51448751e9440c273a995c4c
Francisco J. Peña <frape@kth.se>
2022-10-31 16:03:18 +0100

    Updated the generate_ndwi.py module to load variables from the .env file

--- wetlands/generate_ndwi.py
+++ wetlands/generate_ndwi.py
@@ -1,3 +1,4 @@
+import os
 import time
 
 import numpy as np
@@ -11,11 +12,14 @@ from tqdm import tqdm
 
 from wetlands import utils, viz_utils, geo_utils
 
+from dotenv import load_dotenv
+
 
 def export_ndwi_mask_data(tiles, tif_file):
 
     minValue = 0.5
     maxValue = 1.0
+    export_folder = os.getenv('NDWI_MASK_DIR')
 
     for index in tqdm(range(len(tiles)), total=len(tiles)):
 
@@ -54,12 +58,12 @@ def export_ndwi_mask_data(tiles, tif_file):
             })
 
             # Save the cropped image as a temporary TIFF file.
-            temp_tif = '/tmp/ndwi_mask/{}-ndwi_mask.tif'.format(name)
+            temp_tif = export_folder + '/{}-ndwi_mask.tif'.format(name)
             with rasterio.open(temp_tif, "w", **out_meta) as dest:
                 dest.write(out_image)
 
             # Save the cropped image as a temporary PNG file.
-            temp_png = '/tmp/ndwi_mask/{}-ndwi_mask.png'.format(name)
+            temp_png = export_folder + '/{}-ndwi_mask.png'.format(name)
 
             # Get the color map by name:
             # cm = plt.get_cmap('viridis')
@@ -74,9 +78,10 @@ def export_ndwi_mask_data(tiles, tif_file):
 
 
 def full_cycle():
-    file_name = '/tmp/sweden.geojson'
-    shape_name = 'Sala kommun'
-    tif_file = '/Users/frape/Projects/DeepWetlands/src/deep-wetlands/external/data/{}_new_image.tif'.format(shape_name)
+    file_name = os.getenv('GEOJSON_FILE')
+    shape_name = os.getenv('REGION_NAME')
+    tif_file = os.getenv('TIFF_FILE')
+    print('TIFF file', tif_file)
 
     utils.download_country_boundaries('SWE', 'ADM2', file_name)
     geoboundary = utils.get_region_boundaries(shape_name, file_name)
@@ -87,14 +92,15 @@ def full_cycle():
 
 def full_cycle_with_visualization():
 
-    file_name = '/tmp/sweden.geojson'
-    shape_name = 'Sala kommun'
-    utils.download_country_boundaries('SWE', 'ADM2', file_name)
+    file_name = os.getenv('GEOJSON_FILE')
+    shape_name = os.getenv('REGION_NAME')
+    country_code = os.getenv('COUNTRY_CODE')
+    utils.download_country_boundaries(country_code, 'ADM2', file_name)
     geoboundary = utils.get_region_boundaries(shape_name, file_name)
     utils.show_region_boundaries(geoboundary, shape_name)
-    tif_file = '/Users/frape/Projects/DeepWetlands/src/deep-wetlands/external/data/{}_new_image.tif'.format(shape_name)
+    tif_file = os.getenv('TIFF_FILE')
     viz_utils.visualize_sentinel2_image(geoboundary, shape_name, tif_file)
-    cwd = '/Users/frape/Projects/DeepWetlands/src/deep-wetlands/external/data/Land Use and Land Cover Classification'
+    cwd = os.getenv('CWD_DIR')
     output_file = cwd + '{}.geojson'.format(shape_name)
     tiles = geo_utils.generate_tiles(tif_file, output_file, shape_name, size=64)
     viz_utils.visualize_tiles(geoboundary, shape_name, tif_file, tiles)
@@ -102,17 +108,19 @@ def full_cycle_with_visualization():
     viz_utils.show_crop(tif_file, [tiles.iloc[10]['geometry']])
 
     export_ndwi_mask_data(tiles, tif_file)
-    example_file = '/tmp/ndwi_mask/sala_kommun-1533-ndwi_mask.tif'
+    example_file = os.getenv('NDWI_MASK_DIR') + '/sala_kommun-1533-ndwi_mask.tif'
     viz_utils.visualize_image_from_file(example_file)
 
 
 def main():
+    load_dotenv()
+
     full_cycle()
     # full_cycle_with_visualization()
 
 
-start = time.time()
-main()
-end = time.time()
-total_time = end - start
-print("%s: Total time = %f seconds" % (time.strftime("%Y/%m/%d-%H:%M:%S"), total_time))
+# start = time.time()
+# main()
+# end = time.time()
+# total_time = end - start
+# print("%s: Total time = %f seconds" % (time.strftime("%Y/%m/%d-%H:%M:%S"), total_time))
