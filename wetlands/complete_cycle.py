c3d3c898793b5b7adc71211bfe571308e6721316
Francisco J. Peña <frape@kth.se>
2022-11-03 18:54:56 +0100

    Added a module to execute cycle of the water detection from SAR images platform

---
+++ wetlands/complete_cycle.py
@@ -0,0 +1,21 @@
+import time
+
+from dotenv import load_dotenv
+
+from wetlands import generate_ndwi, generate_sar, train_model, map_wetlands
+
+
+def main():
+    load_dotenv()
+
+    generate_ndwi.full_cycle()
+    generate_sar.full_cycle()
+    train_model.full_cycle()
+    map_wetlands.full_cycle()
+
+
+start = time.time()
+main()
+end = time.time()
+total_time = end - start
+print("%s: Total time = %f seconds" % (time.strftime("%Y/%m/%d-%H:%M:%S"), total_time))
