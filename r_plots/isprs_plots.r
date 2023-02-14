868ec0a608f50b316e59cfbdbeffe0b31d9ca44a
Francisco J. Peña <frape@kth.se>
2023-02-14 08:18:33 +0100

    First version of the plots for the self-supervised paper using R

---
+++ r_plots/isprs_plots.r
@@ -0,0 +1,526 @@
+x <- "Extension	Date
+93808	2018-04-11
+122769	2018-04-23
+134579	2018-04-24
+112105	2018-05-05
+97664	2018-05-17
+75037	2018-05-29
+96309	2018-05-30
+86874	2018-06-04
+68402	2018-06-10
+69336	2018-06-11
+67705	2018-06-16
+66354	2018-06-22
+70050	2018-06-23
+65680	2018-06-28
+64336	2018-07-04
+65160	2018-07-05
+64237	2018-07-10
+63872	2018-07-16
+64705	2018-07-17
+66587	2018-07-22
+64807	2018-07-28
+65056	2018-07-29
+63876	2018-08-03
+67031	2018-08-09
+71225	2018-08-10
+63374	2018-08-15
+63824	2018-08-21
+65554	2018-08-22
+63034	2018-08-27
+63192	2018-09-02
+63785	2018-09-03
+64048	2018-09-08
+63798	2018-09-14
+64487	2018-09-15
+63617	2018-09-20
+63214	2018-09-26
+64931	2018-09-27
+65966	2018-10-02
+64461	2018-10-08
+64822	2018-10-14
+65957	2018-10-20
+65238	2018-10-21
+76307	2018-10-26
+67251	2018-11-01
+67581	2018-11-02
+66603	2018-11-07
+66822	2018-11-13
+90396	2018-11-14
+74985	2018-11-19
+85886	2018-11-25
+127551	2019-04-06
+96212	2019-04-30
+81612	2019-05-12
+68221	2019-05-24
+68839	2019-05-25
+66329	2019-05-30
+65366	2019-06-05
+65408	2019-06-06
+65369	2019-06-11
+64390	2019-06-17
+65079	2019-06-18
+64976	2019-06-23
+64282	2019-06-29
+64406	2019-06-30
+64513	2019-07-05
+64050	2019-07-11
+64496	2019-07-12
+64641	2019-07-17
+64160	2019-07-23
+64762	2019-07-24
+64076	2019-07-29
+63603	2019-08-04
+64386	2019-08-05
+64236	2019-08-10
+64173	2019-08-16
+64629	2019-08-17
+64460	2019-08-22
+64307	2019-08-28
+65628	2019-08-29
+64011	2019-09-03
+63565	2019-09-09
+65598	2019-09-10
+63924	2019-09-15
+63954	2019-09-21
+64611	2019-09-22
+66778	2019-09-27
+65024	2019-10-03
+65664	2019-10-04
+65925	2019-10-09
+67440	2019-10-15
+67606	2019-10-16
+68402	2019-10-21
+85245	2019-10-27
+106119	2019-10-28
+91301	2019-11-02
+90076	2019-11-08
+92306	2019-11-20
+88872	2020-04-12
+92323	2020-04-24
+93278	2020-05-06
+77198	2020-05-18
+70291	2020-05-19
+67839	2020-05-24
+67201	2020-05-30
+63667	2020-05-31
+66845	2020-06-05
+63101	2020-06-11
+62150	2020-06-12
+62325	2020-06-17
+62755	2020-06-23
+61979	2020-06-24
+62292	2020-06-29
+62551	2020-07-05
+61382	2020-07-06
+62213	2020-07-11
+62596	2020-07-17
+61510	2020-07-18
+62516	2020-07-23
+62248	2020-07-29
+61550	2020-07-30
+62330	2020-08-04
+62273	2020-08-10
+61137	2020-08-11
+61714	2020-08-16
+61764	2020-08-22
+60037	2020-08-23
+62322	2020-08-28
+61381	2020-09-03
+60048	2020-09-04
+62295	2020-09-09
+62534	2020-09-15
+61463	2020-09-16
+62870	2020-09-21
+62842	2020-09-27
+61924	2020-09-28
+63492	2020-10-03
+62904	2020-10-09
+62891	2020-10-10
+64572	2020-10-15
+63235	2020-10-21
+61593	2020-10-22
+63110	2020-10-27
+63954	2020-11-02
+63976	2020-11-03
+67028	2020-11-08
+68631	2020-11-14
+66056	2020-11-15
+77070	2020-11-20
+67953	2020-11-26
+94356	2021-04-07
+100762	2021-04-19
+90859	2021-05-01
+85527	2021-05-13
+79680	2021-05-25
+73093	2021-05-26
+86449	2021-05-31
+78091	2021-06-06
+72183	2021-06-07
+70566	2021-06-12
+71291	2021-06-18
+62998	2021-06-19
+65660	2021-06-24
+62699	2021-06-30
+61276	2021-07-01
+62372	2021-07-06
+62915	2021-07-12
+61816	2021-07-13
+62645	2021-07-18
+62194	2021-07-24
+61525	2021-07-25
+62063	2021-07-30
+62251	2021-08-05
+61322	2021-08-06
+62030	2021-08-11
+62012	2021-08-17
+61189	2021-08-18
+62551	2021-08-23
+62809	2021-08-29
+62341	2021-08-30
+64820	2021-09-04
+65700	2021-09-10
+63657	2021-09-11
+66638	2021-09-16
+64798	2021-09-22
+62341	2021-09-23
+65109	2021-09-28
+63597	2021-10-04
+64084	2021-10-05
+75005	2021-10-10
+80243	2021-10-16
+74147	2021-10-17
+74265	2021-10-22
+64876	2021-10-28
+64086	2021-10-29
+64403	2021-11-03
+79022	2021-11-09
+64368	2021-11-10
+77786	2021-11-15
+86135	2021-11-21
+71029	2022-04-02
+112888	2022-04-26
+95411	2022-05-08
+68539	2022-05-20
+65941	2022-06-01
+64012	2022-06-13
+63459	2022-06-25
+62461	2022-07-07
+62251	2022-07-19
+62534	2022-07-31
+61899	2022-08-12
+63138	2022-09-05
+62356	2022-09-17
+62353	2022-09-29
+62762	2022-10-11
+62900	2022-10-23
+63883	2022-11-04
+72406	2022-11-16
+68008	2022-11-28"
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(lubridate)
+
+# df <- read.table(text = x, sep ="\t", header = TRUE, stringsAsFactors = FALSE)
+df <- read_csv("/tmp/paper_water_estimates_kavsjon.csv",
+               col_types = cols(
+                 Extension = col_double(),
+                 Date = col_date(format = "%Y-%m-%d")
+               ))
+df <- rename(df, Extension = Extension, Date = Date)
+df <- select(df, Extension, Date)
+df <- mutate(df, Date= as.Date(Date, format= "%Y-%m-%d"))
+
+# ndwi_df <- read.csv("/tmp/paper_ndwi_water_estimates.csv", select = c("Water", "Date"), col.names = c("Extension", "Date"))
+ndwi_df <- read_csv("/tmp/paper_ndwi_water_estimates_kavsjon.csv",
+               col_types = cols(
+                 Water = col_double(),
+                 Date = col_date(format = "%Y-%m-%d")
+               ))
+ndwi_df <- rename(ndwi_df, Extension = Water, Date = Date)
+ndwi_df <- select(ndwi_df, Extension, Date)
+ndwi_df <- mutate(ndwi_df, Date= as.Date(Date, format= "%Y-%m-%d"))
+
+# df <- summarize(summary_variable = sum(value))
+
+# df <- group_by(month = lubridate::floor_date(df.Date, "month"))
+
+# df <- df %>%
+#   mutate(month = format(Date, "%m"), year_month = format(Date, "%Y-%m"), year = format(Date, "%Y")) %>%
+#   group_by(year_month) %>%
+#   summarise(min = min(Extension), max = max(Extension), mean = mean(Extension))
+
+df_grouped <- df %>%
+  mutate(month = format(Date, "%m"), year_month = format(Date, "%Y-%m"), year = format(Date, "%Y")) %>%
+  group_by(year_month) %>%
+  summarise(min = min(Extension), max = max(Extension), mean = mean(Extension))
+
+df_grouped$year_month <- as.Date(paste0(df_grouped$year_month, "-01"), format = "%Y-%m-%d")
+
+
+ndwi_df_grouped <- ndwi_df %>%
+  mutate(month = format(Date, "%m"), year_month = format(Date, "%Y-%m"), year = format(Date, "%Y")) %>%
+  group_by(year_month) %>%
+  summarise(min = min(Extension), max = max(Extension), mean = mean(Extension))
+
+ndwi_df_grouped$year_month <- as.Date(paste0(ndwi_df_grouped$year_month, "-01"), format = "%Y-%m-%d")
+
+df_grouped <- mutate(df_grouped, method = "SAR")
+ndwi_df_grouped <- mutate(ndwi_df_grouped, method = "NDWI")
+
+df_merged <- bind_rows(df_grouped, ndwi_df_grouped)
+
+# seasons = function(x){
+#   if(x %in% 2:4) return("Spring")
+#   if(x %in% 5:7) return("Summer")
+#   if(x %in% 8:10) return("Fall")
+#   if(x %in% c(11,12,1)) return("Winter")
+#
+# }
+#
+# season_start = function(x){
+#   if(x %in% 2:4) return("Spring")
+#   if(x %in% 5:7) return("Summer")
+#   if(x %in% 8:10) return("Fall")
+#   if(x %in% c(11,12,1)) return("Winter")
+#
+# }
+
+# Define the function to get the season
+# get_season <- function(date) {
+#   month <- month(date)
+#   if (month >= 3 & month <= 5) {
+#     return("Spring")
+#   } else if (month >= 6 & month <= 8) {
+#     return("Summer")
+#   } else if (month >= 9 & month <= 11) {
+#     return("Fall")
+#   } else {
+#     return("Winter")
+#   }
+# }
+
+# Define the function to get the start and end dates of the season
+get_season_dates <- function(date) {
+  year <- year(date)
+  season <- get_season(date)
+  if (season == "Spring") {
+    start_date <- as.Date(paste0(year, "-03-01"))
+    end_date <- as.Date(paste0(year, "-05-31"))
+  } else if (season == "Summer") {
+    start_date <- as.Date(paste0(year, "-06-01"))
+    end_date <- as.Date(paste0(year, "-08-31"))
+  } else if (season == "Fall") {
+    start_date <- as.Date(paste0(year, "-09-01"))
+    end_date <- as.Date(paste0(year, "-11-30"))
+  } else {
+    start_date <- as.Date(paste0(year, "-12-01"))
+    if (year == max(year(df$date))) {
+      end_date <- as.Date("2022-02-28")
+    } else {
+      end_date <- as.Date(paste0(year+1, "-02-28"))
+    }
+  }
+  return(c(start_date, end_date))
+}
+
+
+# Define the function to get the season
+get_season <- function(date) {
+  month <- month(date)
+  if (month >= 3 & month <= 5) {
+    return("Spring")
+  } else if (month >= 6 & month <= 8) {
+    return("Summer")
+  } else if (month >= 9 & month <= 11) {
+    return("Fall")
+  } else {
+    return("Winter")
+  }
+}
+
+# Define the function to get the start and end dates of the season
+get_season_dates <- function(date) {
+  year <- year(date)
+  season <- get_season(date)
+  if (season == "Spring") {
+    start_date <- as.Date(paste0(year, "-03-01"))
+    end_date <- as.Date(paste0(year, "-05-31"))
+  } else if (season == "Summer") {
+    start_date <- as.Date(paste0(year, "-06-01"))
+    end_date <- as.Date(paste0(year, "-08-31"))
+  } else if (season == "Fall") {
+    start_date <- as.Date(paste0(year, "-09-01"))
+    end_date <- as.Date(paste0(year, "-11-30"))
+  } else {
+    start_date <- as.Date(paste0(year, "-12-01"))
+    if (year == max(year(df$date))) {
+      end_date <- as.Date("2022-02-28")
+    } else {
+      end_date <- as.Date(paste0(year+1, "-02-28"))
+    }
+  }
+  return(c(start_date, end_date))
+}
+
+# Add the season, start date, and end date columns to the data frame
+df_merged$season <- sapply(df_merged$year_month, get_season)
+df_merged[, c("start_date", "end_date")] <- t(sapply(df_merged$year_month, get_season_dates))
+
+df_merged$start_date <- as.Date(df_merged$start_date, origin = "1970-01-01")
+df_merged$end_date <- as.Date(df_merged$end_date, origin = "1970-01-01")
+
+
+
+
+# Add the season, start date, and end date columns to the data frame
+# df_merged$season <- sapply(df_merged$year_month, get_season)
+#   df_merged[, c("start_date", "end_date")] <- t(sapply(df_merged$date, get_season_dates))
+
+# df_merged$max_date <- df_merged$year_month %m+% months(1) -1
+
+# df_merged$Season = sapply(month(df_merged$year_month), seasons)
+
+ggplot(df_grouped, aes(x = year_month, y = min)) +
+  geom_line() +
+  xlab("Date") +
+  ylab("Min Extension") +
+  ggtitle("Line Chart")
+
+
+ggplot(df_grouped, aes(x = year_month, y = min)) +
+  geom_bar(stat = "identity") +
+  xlab("Date") +
+  ylab("Min Extension") +
+  ggtitle("Bar Plot")
+
+ggplot(df_merged, aes(x = year_month, y = min, fill = method)) +
+  geom_bar(stat = "identity", position="dodge") +
+  xlab("Category") +
+  ylab("Value") +
+  ggtitle("Bar Chart")
+
+ggplot(df_merged, aes(x = year_month, y = min, color = method)) +
+  geom_point() +
+  xlab("Date") +
+  ylab("Min Extension") +
+  ggtitle("Scatter Plot")
+
+p <- ggplot(data = df_merged, aes(x = year_month, y = min)) +
+  geom_rect(aes(xmin = as.Date("2018-06-01"), xmax = as.Date("2018-09-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightsalmon", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2019-06-01"), xmax = as.Date("2019-09-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightsalmon", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2020-06-01"), xmax = as.Date("2020-09-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightsalmon", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2021-06-01"), xmax = as.Date("2021-09-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightsalmon", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2022-06-01"), xmax = as.Date("2022-09-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightsalmon", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2018-03-01"), xmax = as.Date("2018-06-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2019-03-01"), xmax = as.Date("2019-06-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2020-03-01"), xmax = as.Date("2020-06-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2021-03-01"), xmax = as.Date("2021-06-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2022-03-01"), xmax = as.Date("2022-06-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgreen", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2018-09-01"), xmax = as.Date("2018-12-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgoldenrod", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2019-09-01"), xmax = as.Date("2019-12-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgoldenrod", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2020-09-01"), xmax = as.Date("2020-12-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgoldenrod", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2021-09-01"), xmax = as.Date("2021-12-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgoldenrod", alpha = 0.01) +
+  geom_rect(aes(xmin = as.Date("2022-09-01"), xmax = as.Date("2022-12-01"),
+                ymin = -Inf, ymax = Inf),
+            fill = "lightgoldenrod", alpha = 0.01) +
+  # geom_rect(aes(xmin = as.Date("2018-12-01"), xmax = as.Date("2019-03-01"),
+  #               ymin = -Inf, ymax = Inf),
+  #           fill = "lightblue", alpha = 0.01) +
+  # geom_rect(aes(xmin = as.Date("2019-12-01"), xmax = as.Date("2020-03-01"),
+  #               ymin = -Inf, ymax = Inf),
+  #           fill = "lightblue", alpha = 0.01) +
+  # geom_rect(aes(xmin = as.Date("2020-12-01"), xmax = as.Date("2021-03-01"),
+  #               ymin = -Inf, ymax = Inf),
+  #           fill = "lightblue", alpha = 0.01) +
+  # geom_rect(aes(xmin = as.Date("2021-12-01"), xmax = as.Date("2022-03-01"),
+  #               ymin = -Inf, ymax = Inf),
+  #           fill = "lightblue", alpha = 0.01) +
+  geom_point(aes(color = method), size = 3) +
+  geom_line(aes(group = method, color = method)) +
+  # scale_color_manual(values = c("red", "blue", "green")) +
+  # scale_fill_manual(values=c("lightgreen","lightsalmon","lightgoldenrod"),
+  #   name="Season",
+  #   labels=c("Spring","Summer","Autumn")) +
+  scale_fill_manual(values=c("lightsalmon"), labels=c("lightsalmon")) +
+  # scale_fill_manual(values=c("lightgreen","lightsalmon","lightgoldenrod"), name = "Response", labels = c("Active", "Placebo", "FFFF"))
+  # scale_fill_manual('Highlight this',
+  #                   values = c("lightsalmon","lightgreen","lightgoldenrod"),
+  #                   guide = guide_legend(override.aes = list(alpha = 1))) +
+  # guides(color=guide_legend("Method"),fill=guide_legend("Season")) +
+  # guides(color = guide_legend(nrow = 2, override.aes = list(linetype = 0)))
+  # guides(fill = guide_legend(title.position = "top", title.hjust = 0.5)) +
+  labs(x="Date", y="Water extension (m2)")
+
+print(p)
+
+
+ggplot(data = df_merged, aes(x = year_month, y = min)) +
+  # geom_rect(aes(xmin = year_month, xmax = max_date, fill = Season,
+  #               ymin = -Inf, ymax = Inf),
+  #           fill = c("lightgreen", "lightsalmon", "lightgoldenrod"), alpha = 0.5) +
+  geom_rect(
+    data = df_merged, # or rect_data for actual season end boundary
+    mapping = aes(
+      xmin = start_date,
+      xmax = end_date,
+      ymin = -Inf,
+      ymax = Inf,
+      fill = season
+    ), alpha = 0.5
+  ) +
+  geom_point(aes(color = method), size = 3) +
+  geom_line(aes(group = method, color = method)) +
+  scale_color_manual(values = c("red", "blue", "green"), name="Method") +
+  scale_fill_manual(values=c("lightgoldenrod","lightgreen","lightsalmon"), name="Season") +
+  labs(x="Date", y="Water extension (m2)")
+
+  # + theme_set(theme_gray() + theme(legend.key=element_blank()))
+  # + theme_set(theme_bw() + theme(legend.key=element_blank()))
+
+
+# ggplotly(p)
+
+ggplot(df_merged, aes(x = year_month, y = min)) +
+  geom_rect(aes(xmin = year_month, xmax = dplyr::lead(year_month), ymin = -0.5, ymax = Inf, fill = group),
+            alpha = 0.5) +
+  geom_point(size = 2.5) +
+  theme_classic()
+
+# df_grouped$year_month <- as.Date(df_grouped$year_month, format = "%Y-%m")
+# df_grouped$year_month <- as.Date(df_grouped$year_month, format = "%Y-%m")
+
+
+df_merged
+
