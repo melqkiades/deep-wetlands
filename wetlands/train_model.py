11b400555982f445c0f5c536213fcb52b30b0fdd
Francisco J. Peña <frape@kth.se>
2023-02-27 18:13:05 +0100

    Added the the NDWI input type (between mask or range) as a hyperparamemter

--- wetlands/train_model.py
+++ wetlands/train_model.py
@@ -295,11 +295,13 @@ def full_cycle():
     polarization = os.getenv('SAR_POLARIZATION')
     orbit_pass = os.getenv('ORBIT_PASS')
     patch_size = int(os.getenv('PATCH_SIZE'))
+    ndwi_input = int(os.getenv('NDWI_INPUT'))
 
     config = {
         "learning_rate": learning_rate,
         "epochs": n_epochs,
         "patch_size": patch_size,
+        "ndwi_input": ndwi_input,
         "batch_size": batch_size,
         "num_workers": num_workers,
         "random_seed": seed,
