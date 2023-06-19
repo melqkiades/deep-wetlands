5a64ca72665ab8f70b7ca0efd8ff8f61cf5c6585
Francisco J. Peña <frape@kth.se>
2023-06-19 15:05:50 +0200

    Made a small update on the U-Net implementation

--- model/unet.py
+++ model/unet.py
@@ -51,9 +51,11 @@ class Unet(nn.Module):
     def block(self, in_channels, out_channels):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            # nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            # nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU()
+            nn.ReLU(inplace=True)
         )
