425430c1b4537796902fe4cc810164c6e767694f
Francisco J. Peña <frape@kth.se>
2023-06-19 14:23:26 +0200

    Extracted the U-Net implementation from the train_model.py module

--- wetlands/train_model.py
+++ wetlands/train_model.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import rasterio as rio
 
+from model.unet import Unet
 from wetlands import utils
 from wetlands.jaccard_similarity import calculate_intersection_over_union
 
@@ -87,63 +88,6 @@ def visualize_batch(batch, batch_size):
 
     plt.tight_layout()
     plt.show()
-
-
-class Unet(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1, features=32):
-        super().__init__()
-
-        self.encoder1 = self.block(in_channels, features)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder2 = self.block(features, features*2)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder3 = self.block(features*2, features*4)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.encoder4 = self.block(features*4, features*8)
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.bottleneck = self.block(features*8, features*16)
-
-        self.upconv4 = nn.ConvTranspose2d(features*16, features*8, kernel_size=2, stride=2)
-        self.decoder4 = self.block(features*16, features*8)
-        self.upconv3 = nn.ConvTranspose2d(features*8, features*4, kernel_size=2, stride=2)
-        self.decoder3 = self.block(features*8, features*4)
-        self.upconv2 = nn.ConvTranspose2d(features*4, features*2, kernel_size=2, stride=2)
-        self.decoder2 = self.block(features*4, features*2)
-        self.upconv1 = nn.ConvTranspose2d(features*2, features, kernel_size=2, stride=2)
-        self.decoder1 = self.block(features*2, features)
-
-        self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
-        )
-
-    def forward(self, x):
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(self.pool1(enc1))
-        enc3 = self.encoder3(self.pool2(enc2))
-        enc4 = self.encoder4(self.pool3(enc3))
-
-        bottleneck = self.bottleneck(self.pool4(enc4))
-
-        dec4 = self.decoder4(torch.cat((self.upconv4(bottleneck), enc4), dim=1))
-        dec3 = self.decoder3(torch.cat((self.upconv3(dec4), enc3), dim=1))
-        dec2 = self.decoder2(torch.cat((self.upconv2(dec3), enc2), dim=1))
-        dec1 = self.decoder1(torch.cat((self.upconv1(dec2), enc1), dim=1))
-
-        return torch.sigmoid(self.conv(dec1))
-
-    def block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-      )
-
-
-
 
 
 class DiceLoss(nn.Module):
