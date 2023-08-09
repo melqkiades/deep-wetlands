1b0ff785759d15ff2adc4dbda528c745c974d9ab
Francisco J. Peña <frape@kth.se>
2023-08-09 18:42:24 +0200

    Added a variety of methods to the model_factory.py module based on the segmentation_models_pytorch library

--- model/model_factory.py
+++ model/model_factory.py
@@ -4,13 +4,20 @@ import torch
 
 from model.unet import Unet
 
+import segmentation_models_pytorch as smp
+
 
 def create_model(model_name):
     unet_init_dim = int(os.getenv('UNET_INIT_DIM'))
     unet_blocks = int(os.getenv('UNET_BLOCKS'))
 
     models_dict = {
-        'unet': Unet(in_channels=1, out_channels=1, init_dim=unet_init_dim, num_blocks=unet_blocks)
+        'unet': Unet(in_channels=1, out_channels=1, init_dim=unet_init_dim, num_blocks=unet_blocks),
+        'unet_smp':  smp.Unet(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1),
+        # 'unet2':  smp.Unet(encoder_name="inceptionv4", encoder_weights=None, in_channels=1, classes=1),
+        'unet++':  smp.UnetPlusPlus(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1),
+        'deeplabv3':  smp.DeepLabV3(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1),
+        'deeplabv3+':  smp.DeepLabV3Plus(encoder_name="resnet34", encoder_weights=None, in_channels=1, classes=1),
     }
 
     if model_name not in models_dict.keys():
