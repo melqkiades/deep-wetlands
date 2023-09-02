5c6fb51d3c6c3a737fe86b3e43984fc9d8af3c94
Francisco J. Peña <frape@kth.se>
2023-09-02 09:56:04 +0200

    Fixed tiny bug from the previous commit

--- wetlands/viz_utils.py
+++ wetlands/viz_utils.py
@@ -106,7 +106,7 @@ def convert_ndwi_tiff_to_png(tiff_file, out_file, band):
     image_array[image_array <= 0.5] = 0.0
 
     # plt.imshow(image_array)
-    plt.imsave(out_file + '_color.png', image_array)
+    plt.imsave(out_file.replace('.png', '_color.png'), image_array)
     img = Image.fromarray(numpy.uint8(image_array * 255), 'L')
 
     # Crop the annotated image to fit predicted image size
