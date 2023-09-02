2d87ccded8525bbe08f4b73d98b7097c7cc7270d
Francisco J. Peña <frape@kth.se>
2023-09-02 12:26:19 +0200

    Updated the plant_random_seed() function from the utils.py module

--- wetlands/utils.py
+++ wetlands/utils.py
@@ -14,10 +14,14 @@ from matplotlib import pyplot as plt
 
 
 def plant_random_seed(seed):
+    os.environ["PYTHONHASHSEED"] = str(seed)
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def get_device():
