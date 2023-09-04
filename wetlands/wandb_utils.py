653acb0a461e07c8a3b5b067ddc0d149ee7d57fc
Francisco J. Peña <frape@kth.se>
2023-09-04 15:10:20 +0200

    Small refactor to the wandb_utils.py module

--- wetlands/wandb_utils.py
+++ wetlands/wandb_utils.py
@@ -4,13 +4,20 @@ import wandb
 from dotenv import load_dotenv
 
 
+def get_run_name():
+    load_dotenv()
+
+    run_name = os.getenv("RUN_NAME") if wandb.run is None else wandb.run.name
+
+    return run_name
+
+
 def get_model_path():
     load_dotenv()
 
     model_dir = os.getenv('MODELS_DIR')
-    model_name = os.getenv("MODEL_NAME")
-    run_name = os.getenv("RUN_NAME") if wandb.run is None else wandb.run.name
-    model_file = f'{run_name}_{model_name}.pth'
+    run_name = get_run_name()
+    model_file = f'{run_name}_best_model.pth'
     model_path = os.path.join(model_dir, model_file)
 
     return model_path
